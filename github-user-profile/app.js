f17b0802204ba3d665c78d0888302808afd80eb6
Kevin Kendle <kevin.kendle09@gmail.com>
2020-12-05 23:04:05 -0600

    add axios and make base connection

--- github-user-profile/app.js
+++ github-user-profile/app.js
@@ -1,16 +1,28 @@
 let GithubUserProfile = {
-    template: '#github-user-profile-template',
-    props: {
-        username: {
-            type: String,
-            required: true
-        }
+  template: '#github-user-profile-template',
+  props: {
+    username: {
+      type: String,
+      required: true
     }
+  },
+  created() {
+    axios.get('https://api.github.com/users/' + this.username)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
 }
 
 new Vue({
-    el: '#app',
-    components: {
-        'github-user-profile': GithubUserProfile
-    }
+  el: '#app',
+  components: {
+    'github-user-profile': GithubUserProfile
+  }
 })
