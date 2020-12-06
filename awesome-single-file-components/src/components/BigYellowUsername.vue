9a308ace450f4d37ed324d1d35f31b8879f1e9e0
Kevin Kendle <kevin.kendle09@gmail.com>
2020-12-06 17:27:29 -0600

    sample single file component

---
+++ awesome-single-file-components/src/components/BigYellowUsername.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="username">{{username}}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .username {
+    color: #666610;
+    font-size: 5rem;
+  }
+</style>
