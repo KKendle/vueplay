ce7da6e6ad3f8510f342dd9a4ca239cc64e317c7
Kevin Kendle <kevin.kendle09@gmail.com>
2020-12-05 22:22:41 -0600

    list out requirements

--- github-user-profile/app.js
+++ github-user-profile/app.js
@@ -1,3 +1,10 @@
+let GithubUserProfile = {
+    template: '#github-user-profile-template'
+}
+
 new Vue({
     el: '#app',
+    components: {
+        'github-user-profile': GithubUserProfile
+    }
 })
